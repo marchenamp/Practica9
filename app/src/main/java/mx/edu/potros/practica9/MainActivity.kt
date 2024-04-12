924b1d89208b860e2f941d0f990641cb9b796130
DESKTOP-4RU6P74\march <misael.marchena233418@potros.itson.edu.mx>
2024-04-11 17:23:25 -0700

    Subir proyecto final

---
+++ app/src/main/java/mx/edu/potros/practica9/MainActivity.kt
@@ -0,0 +1,146 @@
+package mx.edu.potros.practica9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+    private val userRef = FirebaseDatabase.getInstance().getReference("Users")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var btnSave: Button = findViewById(R.id.save_button) as Button
+        btnSave.setOnClickListener { saveMarkFromForm() }
+
+        userRef.addChildEventListener(object: ChildEventListener{
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildAdded(dataSnapshot: DataSnapshot, p1: String?) {
+                val value = dataSnapshot.getValue()
+
+                if (value is String) {
+                } else if (value is User) {
+                    val usuario = value
+
+                    if(usuario!=null) {writeMark(usuario)}
+                }
+            }
+        })
+    }
+
+    private fun saveMarkFromForm(){
+        var name:EditText=findViewById(R.id.et_name) as EditText
+        var lastName:EditText=findViewById(R.id.et_lastName) as EditText
+        var age:EditText=findViewById(R.id.et_age) as EditText
+
+        val usuario = User(
+            name.text.toString(),
+            lastName.text.toString(),
+            age.text.toString()
+        )
+
+        userRef.push().setValue(usuario)
+
+    }
+
+    private fun writeMark(mark: User){
+        var listV:TextView=findViewById(R.id.list_textView) as TextView
+        val text = listV.text.toString() + mark.toString() + "\n"
+        listV.text = text
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
